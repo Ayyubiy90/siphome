f0395537ea6a3373cb44b968b6204ff5e556b69f
AYB <ayyubiy67@gmail.com>
2024-07-22 12:59:15 +0100

    Update Topnav component to use logo from public/assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import Topnav from "./components/Topnav";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Update from "./components/Update";
-import Services from "./components/Services";
-import Testimonial from "./components/Testimonial";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Topnav />
-      <Hero />
-      <Features />
-      <Update />
-      <Services />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      {/* Other components will go here */}
     </div>
   );
 }
--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,9 +1,34 @@
 import React from "react";
-
+import "../styles/components/Topnav.css";
 function Topnav() {
   return (
-    <nav>
-      <h1>Topnav</h1>
+    <nav className="topnav">
+      <img src="/assets/logo.png" alt="Siphome Logo" className="logo" />
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#faq">FAQ</a>
+        </li>
+      </ul>
+      <div className="auth-links">
+        <a href="#sign-in" className="sign-in">
+          Sign In
+        </a>
+        <a href="#get-started" className="get-started">
+          Get Started
+        </a>
+      </div>
     </nav>
   );
 }
