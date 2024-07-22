8f7709c5900f4fabaf998757c35193bad307df18
AYB <ayyubiy67@gmail.com>
2024-07-22 12:13:18 +0100

    Description of the changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Topnav from "./components/Topnav";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Update from "./components/Update";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topnav />
+      <Hero />
+      <Features />
+      <Update />
+      <Services />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Topnav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Topnav() {
+  return (
+    <nav>
+      <h1>Topnav</h1>
+    </nav>
+  );
+}
+
+export default Topnav;
