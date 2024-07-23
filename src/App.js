35fdbc183b6dec319dfb2c043b7a1331b8bd4044
AYB <ayyubiy67@gmail.com>
2024-07-23 14:57:05 +0100

    Update Hero section CSS to extend background image and cover Topnav
    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Topnav from "./components/Topnav";
+import Hero from "./components/Hero";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Topnav />
+      <Hero />
       {/* Other components will go here */}
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/components/Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Your Heading Here</h1>
+        {/* You can add more content here if needed */}
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
