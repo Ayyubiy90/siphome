5564de3242fac9bcd12cf84995a6b05a1a7e6de4
AYB <ayyubiy67@gmail.com>
2024-07-23 17:02:58 +0100

    Added onClick handler to the buttons to verify if the buttons are responding...

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,10 @@ import React from "react";
 import "../styles/components/Hero.css";
 
 function Hero() {
+  const handleClick = (event) => {
+    event.preventDefault();
+    alert("Button clicked!");
+  };
   return (
     <section className="hero">
       <div className="hero-content">
@@ -9,16 +13,16 @@ function Hero() {
           Imagine your <br /> home smart enough <br />
           <span className="highlight">to take care</span> of itself.
         </h1>
-        <p>
-          Turn your home into a smart home today with a simple & <br />{" "}
-          affordable upgrade. Discover the upgrade that automates <br /> home
-          maintenance.
+        <p className="subtext">
+          Turn your home into a smart home today with a simple & <br />
+          affordable upgrade. Discover the upgrade that automates <br />
+          home maintenance.
         </p>
         <div className="hero-buttons">
-          <a href="#get-started" className="hero-button">
+          <a href="#get-started" className="hero-button" onClick={handleClick}>
             Get Started
           </a>
-          <a href="#learn-more" className="hero-button">
+          <a href="#learn-more" className="hero-button" onClick={handleClick}>
             Learn More
           </a>
         </div>
