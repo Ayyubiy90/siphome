520bd5f3fc47c312901dd122b0c38733881ce3a0
AYB <ayyubiy67@gmail.com>
2024-07-23 12:36:04 +0100

    Add responsive navigation menu

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/Topnav.css";
 
 function Topnav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="topnav">
       <img src="/assets/logo.png" alt="Siphome Logo" className="logo" />
-      <ul className="nav-links">
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
         <li>
           <a href="#home">Home</a>
         </li>
@@ -22,7 +28,7 @@ function Topnav() {
           <a href="#faq">FAQ</a>
         </li>
       </ul>
-      <div className="auth-links">
+      <div className={`auth-links ${menuOpen ? "open" : ""}`}>
         <a href="#sign-in" className="sign-in">
           Sign In
         </a>
@@ -30,6 +36,9 @@ function Topnav() {
           Get Started
         </a>
       </div>
+      <button className="menu-button" onClick={toggleMenu}>
+        {menuOpen ? "Close" : "Menu"}
+      </button>
     </nav>
   );
 }
