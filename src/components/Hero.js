4d894c7c25d2f9a9952bdf3010a3048c85ffcc5e
AYB <ayyubiy67@gmail.com>
2024-07-23 15:38:33 +0100

    Added Get Started button and Learn More button also Also added their styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,11 +9,19 @@ function Hero() {
           Imagine your <br /> home smart enough <br />
           <span className="highlight">to take care</span> of itself.
         </h1>
-        <p className="subtext">
-          Turn your home into a smarthome today with a simple & <br />{" "}
+        <p>
+          Turn your home into a smart home today with a simple & <br />{" "}
           affordable upgrade. Discover the upgrade that automates <br /> home
           maintenance.
         </p>
+        <div className="hero-buttons">
+          <a href="#get-started" className="hero-button">
+            Get Started
+          </a>
+          <a href="#learn-more" className="hero-button">
+            Learn More
+          </a>
+        </div>
       </div>
     </section>
   );
