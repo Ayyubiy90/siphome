9fe2fafab030a8a1470d05841a11cf083d067a3e
AYB <ayyubiy67@gmail.com>
2024-07-25 15:33:31 +0100

    Add service section with 9 divs and icons, make responsive

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "../styles/components/Services.css";
 import illustrationServices from "../assets/services-illustration.png";
+import iconMobileCommandManagement from "../assets/icon-mobile-command-management.png";
+import iconEquipmentOptimizer from "../assets/icon-equipment-optimizer.png";
+import iconLifecyclePrediction from "../assets/icon-lifecycle-prediction.png";
+import iconEnergyOptimizer from "../assets/icon-energy-optimizer.png";
+import iconTroubleshootingGuide from "../assets/icon-troubleshooting-guide.png";
+import iconSmartDiagnostics from "../assets/icon-smart-diagnostics.png";
+import iconVitalEquipmentScan from "../assets/icon-vital-equipment-scan.png";
+import iconSmartAlerts from "../assets/icon-smart-alerts.png";
+import iconPreventativeMaintenance from "../assets/icon-preventative-maintenance.png";
 
 const Service = () => {
+  const services = [
+    { icon: iconMobileCommandManagement, text: "Mobile Command Management" },
+    { icon: iconEquipmentOptimizer, text: "Equipment Optimizer" },
+    { icon: iconLifecyclePrediction, text: "Lifestyle Prediction" },
+    { icon: iconTroubleshootingGuide, text: "Troubleshooting Guide" },
+    { icon: iconEnergyOptimizer, text: "Energy Optimizer" },
+    { icon: iconSmartDiagnostics, text: "Smart Diagnostics" },
+    { icon: iconVitalEquipmentScan, text: "Vital Equipment Scan" },
+    { icon: iconSmartAlerts, text: "Smart Alerts" },
+    { icon: iconPreventativeMaintenance, text: "Preventative Maintenance" },
+  ];
+
   return (
     <section className="service">
       <div className="service-content">
@@ -19,6 +40,14 @@ const Service = () => {
           alt="Illustration of services"
           className="service-image"
         />
+        <div className="service-icons-container">
+          {services.map((service, index) => (
+            <div className="service-icon-box" key={index}>
+              <img src={service.icon} alt="Icon" className="service-icon" />
+              <p className="service-icon-text">{service.text}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
