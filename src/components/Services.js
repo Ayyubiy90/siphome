22e4a903ee8cf0c58521f183080086ef695871c9
AYB <ayyubiy67@gmail.com>
2024-07-25 14:47:37 +0100

    Add illustration image under service section content Added some icon to the assets folder

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "../styles/components/Services.css";
+import illustrationServices from "../assets/services-illustration.png";
 
 const Service = () => {
   return (
-    <div className="service">
-      <div className="service-title">SERVICES</div>
-      <div className="service-heading">Bring more systems into the network</div>
-      <p className="service-description">
-        Find out just how far Siphome can lead your home into next-gen
-        intelligence.
-      </p>
-    </div>
+    <section className="service">
+      <div className="service-content">
+        <h2 className="service-title">SERVICES</h2>
+        <h3 className="service-subtitle">
+          Bring more systems into the network
+        </h3>
+        <p className="service-description">
+          Find out just how far Siphome can lead your home into next-gen
+          intelligence.
+        </p>
+        <img
+          src={illustrationServices}
+          alt="Illustration of services"
+          className="service-image"
+        />
+      </div>
+    </section>
   );
 };
 
