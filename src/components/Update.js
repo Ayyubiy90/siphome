0ccbc253ce035c7bb05beae02679238480330250
AYB <ayyubiy67@gmail.com>
2024-07-25 07:52:34 +0100

    Add Update component with background image and styled text, including additional image

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "../styles/components/Update.css";
+import backgroundUpdate from "../assets/background-update.png";
+import updateImage from "../assets/update1.png";
 
 function Update() {
   return (
-    <section className="update">
+    <section
+      className="update"
+      style={{ backgroundImage: `url(${backgroundUpdate})` }}>
       <div className="update-content">
         <h2 className="update-title">UPDATE</h2>
-        <p className="update-subtitle">It starts with an upgrade.</p>
+        <p className="update-subtitle">It starts with an upgrade</p>
+        <div className="update-image-container">
+          <img src={updateImage} alt="Update" className="update-image" />
+        </div>
       </div>
     </section>
   );
